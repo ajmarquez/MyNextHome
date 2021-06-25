9f50eb233e087bbbb286344a108bb6f812a3b159
Abelardo Marquez <abelardo@hey.com>
2021-06-26 00:13:07 +0200

    Add TableView to ListViewController and ListViewDelegate

--- MyNextHome/List/ListViewController.swift
+++ MyNextHome/List/ListViewController.swift
@@ -8,6 +8,32 @@
 import Foundation
 import UIKit
 
+protocol ListViewDelegate {
+    func didSelectItem()
+    func reloadListData()
+}
+
+
 class ListViewController: UIViewController {
+    
+    var listDelegate: ListViewDelegate!
+    let tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTableView()
+
+        
+    }
+    
+    func setTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
     
 }
---
+++ MyNextHome/List/ListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListViewModel.swift
+//  MyNextHome
+//
+//  Created by Abelardo Marquez on 25.06.21.
+//
+
+import Foundation
